757625891e9ae2b0872bd5e74b7109a096e9ddbd
masonCalmAndCode <t100210022002@gmail.com>
2022-11-22 10:51:03 +0800

    test equation: add k log

--- section3/SimpleSwap/test/getAmountOut.js
+++ section3/SimpleSwap/test/getAmountOut.js
@@ -1,38 +1,56 @@
-// const k = 10 ** 18
-// const sum = 100000000
-// const amount = [1]
-// while (amount.length < sum) {
-//     amount.push(amount.length + 1)
-// }
-// amount.forEach(a => {
-//     const b = _getAmountOut1(a, Math.sqrt(k), Math.sqrt(k))
-//     const c = _getAmountOut2(a, Math.sqrt(k), Math.sqrt(k))
-//     const d = _getAmountOut3(a, Math.sqrt(k), Math.sqrt(k))
-//     if (b != d) {
-//         console.group(`When k = ${k}, amount= ${a}:`)
-//         console.log("❌")
-//         console.log("1️⃣", b, ", k=", (Math.sqrt(k) + a) * (Math.sqrt(k) - b))
-//         console.log("2️⃣", c, ", k=", (Math.sqrt(k) + a) * (Math.sqrt(k) - c))
-//         console.log("3️⃣", d, ", k=", (Math.sqrt(k) + a) * (Math.sqrt(k) - d))
-//         console.groupEnd(`When k = ${k}, amount= ${a}:`)
-//     }
-// })
+const k = 10 ** 18
+const reserveInput = Math.sqrt(k)
+const reserveOutput = Math.sqrt(k)
+const sum = 100000000
+const amount = [1]
+while (amount.length < sum) {
+    amount.push(amount.length + 1)
+}
+amount.forEach(a => {
+    const b = _getAmountOut1(a, reserveInput, reserveOutput)
+    const c = _getAmountOut2(a, reserveInput, reserveOutput)
+    const d = _getAmountOut3(a, reserveInput, reserveOutput)
 
-// function _getAmountOut1(amountIn, reserveInput, reserveOutput) {
-//     return Math.floor((amountIn * reserveOutput) / (reserveInput + amountIn))
-// }
+    if (b != d) {
+        console.group(`When original k = ${k}, amount= ${a}:`)
+        console.log(
+            "1️⃣ AmoutOut:",
+            b,
+            ", Is new k >= old k ?",
+            (Math.sqrt(k) + a) * (Math.sqrt(k) - b) >= k ? "✅" : "❌",
+        )
+        console.log(
+            "2️⃣ AmoutOut:",
+            c,
+            ", Is new k >= old k ?",
+            (Math.sqrt(k) + a) * (Math.sqrt(k) - c) >= k ? "✅" : "❌",
+        )
+        console.log("❌ 1️⃣ != 3️⃣")
+        console.log(
+            "3️⃣ AmoutOut:",
+            d,
+            ", Is new k >= old k ?",
+            (Math.sqrt(k) + a) * (Math.sqrt(k) - d) >= k ? "✅" : "❌",
+        )
+        console.groupEnd(`When original k = ${k}, amount= ${a}:`)
+    }
+})
 
-// function _getAmountOut2(amountIn, reserveInput, reserveOutput) {
-//     const oldK = reserveInput * reserveOutput
-//     const newReserveInput = reserveInput + amountIn
-//     const newReserveOutput = Math.floor(oldK / newReserveInput)
-//     return reserveOutput - newReserveOutput
-// }
+function _getAmountOut1(amountIn, reserveInput, reserveOutput) {
+    return Math.floor((amountIn * reserveOutput) / (reserveInput + amountIn))
+}
 
-// function _getAmountOut3(amountIn, reserveInput, reserveOutput) {
-//     const oldK = reserveInput * reserveOutput
-//     const newReserveInput = reserveInput + amountIn
-//     // @dev (newReserveInput - 1) if for round to 1
-//     const newReserveOutput = Math.floor((oldK + (newReserveInput - 1)) / newReserveInput)
-//     return reserveOutput - newReserveOutput
-// }
+function _getAmountOut2(amountIn, reserveInput, reserveOutput) {
+    const oldK = reserveInput * reserveOutput
+    const newReserveInput = reserveInput + amountIn
+    const newReserveOutput = Math.floor(oldK / newReserveInput)
+    return reserveOutput - newReserveOutput
+}
+
+function _getAmountOut3(amountIn, reserveInput, reserveOutput) {
+    const oldK = reserveInput * reserveOutput
+    const newReserveInput = reserveInput + amountIn
+    // @dev (newReserveInput - 1) if for round to 1
+    const newReserveOutput = Math.floor((oldK + (newReserveInput - 1)) / newReserveInput)
+    return reserveOutput - newReserveOutput
+}
